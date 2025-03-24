49219e7d9199e56f0b33a9e1d7f7688e0a64e7c9
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-03-24 10:30:30 +0300

    Reformatting with black

--- src/processing.py
+++ src/processing.py
@@ -2,23 +2,31 @@ from datetime import datetime
 from typing import List, Dict, Union
 
 
-def filter_by_state(user_data: List[Dict[str, str]], state: str = 'EXECUTED') -> Union[List[Dict[str, str]], str]:
+def filter_by_state(
+    user_data: List[Dict[str, str]], state: str = "EXECUTED"
+) -> Union[List[Dict[str, str]], str]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
- соответствует указанному значению."""
+    соответствует указанному значению."""
     if not user_data:
         return f"State not found"
 
-    if not any(list_state.get('state') == state for list_state in user_data):
+    if not any(list_state.get("state") == state for list_state in user_data):
         return f"State not found"
 
-    filtered_list_state = [list_state for list_state in user_data if list_state.get('state') == state]
+    filtered_list_state = [
+        list_state for list_state in user_data if list_state.get("state") == state
+    ]
     return filtered_list_state
 
 
-def sort_by_date(data_: List[Dict[str, str]], reverse: bool = True) -> str | list[list[dict[str, str]]]:
+def sort_by_date(
+    data_: List[Dict[str, str]], reverse: bool = True
+) -> str | list[list[dict[str, str]]]:
     """Функция возвращает новый список, отсортированный по дате."""
     if not data_:
         return f"There is no data available for filtering"
 
-    filtered_list_date = [sorted(data_, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)]
+    filtered_list_date = [
+        sorted(data_, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
+    ]
     return filtered_list_date
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,34 +3,76 @@ from tests.conftest import get_operation_list
 
 
 def test_filter_by_state(get_operation_list):
-    assert filter_by_state(get_operation_list, 'EXECUTED') == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    assert filter_by_state(get_operation_list, "EXECUTED") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
-    assert filter_by_state(get_operation_list, 'CANCELED') == [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 615064592, 'state': 'CANCELED', 'date': '2018-10-14T08:21:30.419128'},
+    assert filter_by_state(get_operation_list, "CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 615064592, "state": "CANCELED", "date": "2018-10-14T08:21:30.419128"},
     ]
-    assert filter_by_state(get_operation_list, 'ERROR') == "State not found"
+    assert filter_by_state(get_operation_list, "ERROR") == "State not found"
 
-    assert filter_by_state(get_operation_list, '') == "State not found"
+    assert filter_by_state(get_operation_list, "") == "State not found"
 
 
 def test_sort_by_date(get_operation_list):
     assert sort_by_date(get_operation_list) == [
-        [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-         {'id': 615064592, 'state': 'CANCELED', 'date': '2018-10-14T08:21:30.419128'},
-         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]]
+        [
+            {   "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364"},
+            {
+                "id": 615064591,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:33.419441",
+            },
+            {
+                "id": 615064592,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:30.419128",
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+            },
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+            },
+        ]
+    ]
 
     assert sort_by_date(get_operation_list, False) == [
-        [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-         {'id': 615064592, 'state': 'CANCELED', 'date': '2018-10-14T08:21:30.419128'},
-         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]]
+        [
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+            },
+            {
+                "id": 615064592,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:30.419128",
+            },
+            {
+                "id": 615064591,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:33.419441",
+            },
+            {   "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364"},
+        ]
+    ]
 
     assert sort_by_date("") == "There is no data available for filtering"
