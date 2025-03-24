5d887a8ff3d9aaf3ec50b8381178eaa46a786af7
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-03-24 10:16:08 +0300

    Модифицирована функция sort_by_date: добавлена проверка на отсутствие входных данных. Исправлены аннотации.

--- src/processing.py
+++ src/processing.py
@@ -6,7 +6,7 @@ def filter_by_state(user_data: List[Dict[str, str]], state: str = 'EXECUTED') ->
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
  соответствует указанному значению."""
     if not user_data:
-        return "State not found"
+        return f"State not found"
 
     if not any(list_state.get('state') == state for list_state in user_data):
         return f"State not found"
@@ -15,10 +15,10 @@ def filter_by_state(user_data: List[Dict[str, str]], state: str = 'EXECUTED') ->
     return filtered_list_state
 
 
-def sort_by_date(data_: List[Dict[str, str]], reverse: bool = True) -> list[list[dict[str, str]]]:
+def sort_by_date(data_: List[Dict[str, str]], reverse: bool = True) -> str | list[list[dict[str, str]]]:
     """Функция возвращает новый список, отсортированный по дате."""
+    if not data_:
+        return f"There is no data available for filtering"
 
     filtered_list_date = [sorted(data_, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)]
     return filtered_list_date
-
-
