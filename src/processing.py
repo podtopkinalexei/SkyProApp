5525e11e6719cf447ef46484ec008b0a3933a7e2
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-03-24 10:07:06 +0300

    Добавлены аннотации к функциям. Удалены тестовые данные

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from typing import List, Dict, Union
 
 
-def filter_by_state(user_data: list[dict], state: str = 'EXECUTED'):
+def filter_by_state(user_data: List[Dict[str, str]], state: str = 'EXECUTED') -> Union[List[Dict[str, str]], str]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
  соответствует указанному значению."""
     if not user_data:
@@ -14,17 +15,10 @@ def filter_by_state(user_data: list[dict], state: str = 'EXECUTED'):
     return filtered_list_state
 
 
-def sort_by_date(data_: list[dict], reverse=True):
+def sort_by_date(data_: List[Dict[str, str]], reverse: bool = True) -> list[list[dict[str, str]]]:
     """Функция возвращает новый список, отсортированный по дате."""
+
     filtered_list_date = [sorted(data_, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)]
     return filtered_list_date
 
 
-get_operation_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                      {'id': 615064592, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-
-print(filter_by_state(get_operation_list))
