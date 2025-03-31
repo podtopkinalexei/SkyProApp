1ccdade05a815f7526a1b3aa294964b55a980aa6
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-03-31 20:23:16 +0300

    Исправлены аннотации к функциям

--- src/generators.py
+++ src/generators.py
@@ -1,24 +1,29 @@
-from typing import Iterator
+from typing import Any, Dict, Iterator
 
 
-def filter_by_currency(transactions: list[dict], select_currency="USD") -> Iterator[dict]:
+def filter_by_currency(
+    transactions: list[Dict[str, Any]], select_currency: str = "USD"
+) -> Iterator[Dict[str, Any]]:
     """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной."""
 
     for operation in transactions:
-        if operation.get("operationAmount", {}).get("currency", {}).get("code") == select_currency:
+        if (
+            operation.get("operationAmount", {}).get("currency", {}).get("code")
+            == select_currency
+        ):
             yield operation
 
 
-def transaction_descriptions(transactions: list[dict]) -> str:
-    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+def transaction_descriptions(transactions: list[Dict[str, Any]]) -> Iterator[str]:
+    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
 
     for operation in transactions:
         yield operation.get("description", "")
 
 
-def card_number_generator(start: int, end: int) -> str:
+def card_number_generator(start: int, end: int) -> Iterator[str]:
     """Функция генерирует номер банковской карты в заданном диапазоне в формате XXXX XXXX XXXX XXXX,
-     где X — цифра номера карты"""
+    где X — цифра номера карты."""
 
     checks = [
         isinstance(start, int),  # Проверка, что start — целое число
@@ -34,4 +39,4 @@ def card_number_generator(start: int, end: int) -> str:
         for number in gen_number:
             yield f"{number[:4]} {number[4:8]} {number[8:12]} {number[12:16]}"
     else:
-        print(f"Incorrect range")
+        print("Incorrect range")
