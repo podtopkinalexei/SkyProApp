06ba67bf604f30a977b3f08240207d4d113a9fc5
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-03-31 19:56:48 +0300

    Добавлен модуль generators.py c функциями filter_by_currency, transaction_descriptions, card_number_generator

---
+++ src/generators.py
@@ -0,0 +1,37 @@
+from typing import Iterator
+
+
+def filter_by_currency(transactions: list[dict], select_currency="USD") -> Iterator[dict]:
+    """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной."""
+
+    for operation in transactions:
+        if operation.get("operationAmount", {}).get("currency", {}).get("code") == select_currency:
+            yield operation
+
+
+def transaction_descriptions(transactions: list[dict]) -> str:
+    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+
+    for operation in transactions:
+        yield operation.get("description", "")
+
+
+def card_number_generator(start: int, end: int) -> str:
+    """Функция генерирует номер банковской карты в заданном диапазоне в формате XXXX XXXX XXXX XXXX,
+     где X — цифра номера карты"""
+
+    checks = [
+        isinstance(start, int),  # Проверка, что start — целое число
+        isinstance(end, int),  # Проверка, что end — целое число
+        0 <= start <= 9999999999999999,  # Проверка диапазона для start
+        0 <= end <= 9999999999999999,  # Проверка диапазона для end
+        start <= end,  # Проверка, что start не больше end
+    ]
+
+    if all(checks):
+        gen_number = [f"{number:016d}" for number in range(start, end + 1)]
+
+        for number in gen_number:
+            yield f"{number[:4]} {number[4:8]} {number[8:12]} {number[12:16]}"
+    else:
+        print(f"Incorrect range")
