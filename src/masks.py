7f387a27bc29d5ef13ce99f3794ba9c14f11e877
Alexey Podtopkin <podtopkinalexei@mail.ru>
2025-04-29 11:38:38 +0300

    Убран запуск функции make_dir()

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,18 @@
 import logging
 
-from make_log_dir import make_dir
+# from src.make_log_dir import make_dir
+#
+# make_dir()
 
-make_dir()
 default_logs_path_name = "../logs/masks.log"
 
 logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler(default_logs_path_name, mode="w")
 file_handler.setLevel(logging.DEBUG)
-file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -32,10 +35,3 @@ def get_mask_account(bank_account: str) -> str:
     else:
         logger.info("Success")
         return f"**{bank_account[-4:]}"
-
-
-# Проверка работы функций
-# print(get_mask_account("12345678910121231222"))
-# print(get_mask_account("123"))
-# print(get_mask_card_number("1234567890123456"))
-# print(get_mask_card_number("123"))
